02fcfab9c5635e11cf3371f5c2e026af4aa3ea56
Yumeng Liu <lym20041026@gmail.com>
2025-10-08 17:14:41 -0700

    remove unused

--- frontend/src/routes/DriveLayout.tsx
+++ frontend/src/routes/DriveLayout.tsx
@@ -1,29 +1,19 @@
 import ProtectedRoute from "@/components/ProtectedRoute";
 import Sidebar from "@/components/Sidebar";
-import { SUB_ROUTES } from "@/constants";
-import useAuthStore from "@/context/authStore";
-import { Outlet, NavLink, useNavigate } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 
 export default function DriveLayout() {
-	const navigator = useNavigate();
-	const { setToken } = useAuthStore();
-
-	const handleLogout = () => {
-		navigator(SUB_ROUTES.login);
-		setToken(null);
-	};
-
 	return (
 		<ProtectedRoute>
-		  <div className="flex min-h-screen">
-  			{/* Sidebar */}
-  			<Sidebar></Sidebar>
-  
-  			{/* Content area */}
-  			<main className="flex-1 p-6">
-  				<Outlet /> {/* Renders subroute content */}
-  			</main>
-  		</div>
+			<div className="flex min-h-screen">
+				{/* Sidebar */}
+				<Sidebar></Sidebar>
+
+				{/* Content area */}
+				<main className="flex-1 p-6">
+					<Outlet /> {/* Renders subroute content */}
+				</main>
+			</div>
 		</ProtectedRoute>
 	);
 }
